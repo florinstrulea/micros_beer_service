ff3f515a9e1c3a8b639537f7f259d9875d43b5de
Florin STRULEA <florin.strulea@neo-soft.fr>
2022-12-27 11:55:55 +0100

    initial commit

---
+++ src/main/java/beer/micros_beer_service/MicrosBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package beer.micros_beer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beer/micros_beer_service/MicrosBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package beer.micros_beer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
