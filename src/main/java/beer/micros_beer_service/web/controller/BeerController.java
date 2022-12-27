5866d55716b70cc0b36890c6dba5cce6fc992f6c
Florin STRULEA <florin.strulea@neo-soft.fr>
2022-12-27 17:05:51 +0100

    Added file for testing

---
+++ src/main/java/beer/micros_beer_service/web/controller/BeerController.java
@@ -0,0 +1,50 @@
+package beer.micros_beer_service.web.controller;
+
+import beer.micros_beer_service.web.model.BeerDto;
+import beer.micros_beer_service.web.services.BeerService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+  private final BeerService beerService;
+
+  public BeerController(BeerService beerService) {
+    this.beerService = beerService;
+  }
+
+  @GetMapping("/{beerId}")
+  public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+    return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+  }
+
+  @PostMapping("/{beerId}")
+  public ResponseEntity<String> handlePost(@RequestBody BeerDto beerDto) {
+    BeerDto savedDto = beerService.saveNewBeer(beerDto);
+    HttpHeaders httpHeaders = new HttpHeaders();
+    //TODO add hostname to url
+    httpHeaders.add("Location", "/api/v1/beer" + savedDto.getId().toString());
+    return new ResponseEntity<>(httpHeaders, HttpStatus.CREATED);
+  }
+
+  @PutMapping({"/{beerId}"})
+  public ResponseEntity<String> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+
+    beerService.updateBeer(beerId, beerDto);
+
+    return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+  }
+
+  @DeleteMapping({"/{beerId}"})
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+    beerService.deleteById(beerId);
+  }
+
+
+}
