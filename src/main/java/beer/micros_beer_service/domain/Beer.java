338206aac3c70be7d3e80abc2979fbc575250640
Florin STRULEA <florin.strulea@neo-soft.fr>
2022-12-28 17:52:17 +0100

    Added BeerRepository

---
+++ src/main/java/beer/micros_beer_service/domain/Beer.java
@@ -0,0 +1,46 @@
+package beer.micros_beer_service.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.security.Timestamp;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+  @Column(length = 36, columnDefinition = "varchar2", updatable = false, nullable = false)
+  private UUID id;
+
+  @Version
+  private Long version;
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  private Timestamp createdDate;
+
+  @UpdateTimestamp
+  private Timestamp lastModifiedDate;
+  private String beerName;
+  private String beerStyle;
+
+  @Column(unique = true)
+  private String upc;
+  private BigDecimal price;
+  private Integer minOnHand;
+  private Integer quantityToBrew;
+}
---
+++ src/main/java/beer/micros_beer_service/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package beer.micros_beer_service.repositories;
+
+import beer.micros_beer_service.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
